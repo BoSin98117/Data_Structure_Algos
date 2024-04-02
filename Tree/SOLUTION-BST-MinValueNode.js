b0f64e85802365b1c46453d7f634c5fa52302ea1
bo98117 <bosin98117@gmail.com>
2024-04-02 06:46:43 -0700

    Stacks

--- Tree/SOLUTION-BST-MinValueNode.js
+++ Tree/SOLUTION-BST-MinValueNode.js
@@ -5,7 +5,7 @@ class Node {
         this.right = null;
     }
 }
-// BST tree Stacks and Queues and Trees
+// BST tree Stacks and Queues and Trees Stacks
 class BST {
     constructor() {
         this.root = null;
